3f70e353484300eab9848c60a1bd69b8567e6904
aleffericlys <ericlys558@gmail.com>
2024-05-20 13:53:47 -0300

    transformando sidebar em um componente próprio

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,15 @@
 <template>
-	<nav>
-		<div class="leftSideBar">
-			side bar
-		</div>
-		<div class="links">
-			<router-link to="/">
-				<div class="home">
-					Home
-				</div>
-			</router-link>
-			<router-link to="/calculator">
-				<div class="calculator">
-					Calculator
-				</div>
-			</router-link>
-		</div>
-		<div class="RightSidebar">
-			<LoginSideBar />
-		</div>
-	</nav>
+	<Sidebar/>
 	<router-view />
 </template>
 
 <script>
-import LoginSideBar from "@/components/LoginSideBar.vue";
+import Sidebar from "@/components/Sidebar.vue";
 
 export default {
 	name: "App",
 	components: {
-		LoginSideBar,
+		Sidebar,
 	},
 };
 </script>
@@ -42,54 +23,5 @@ export default {
 	color: #2c3e50;
 }
 
-nav {
-	padding-left: 10px;
-	padding-right: 10px;
-	height: 40px;
-	background-color: #d9d9d9;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
 
-
-	.links {
-		display: flex;
-		background-color: #f5f5f5;
-		padding: 3px;
-		width: 60%;
-		height: 80%;
-		margin: 0 auto;
-		border-radius: 5px;
-		align-items: center;
-		justify-content: space-around;
-
-
-		a {
-			background-color: #dcdcdc;
-			color: #1c1c1c;
-			padding: 3px;
-			width: 48%;
-			height: 90%;
-			border-radius: 5px;
-			display: flex;
-			align-items: center;
-			// align-content: center;
-			justify-content: center;
-			text-decoration: none;
-
-			&.router-link-exact-active {
-				color: #ffffff;
-				background-color: #444444;
-				display: flex;
-				align-items: center;
-			}
-
-			// .div {
-			// 	display: flex;
-			// 	background-color: green;
-			// 	height: 100%;
-			// }
-		}
-	}
-}
 </style>
