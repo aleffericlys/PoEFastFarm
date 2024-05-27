80f22a1a4c11c3df7af2dcd5e3b1ef1a0bb7599c
aleffericlys <ericlys558@gmail.com>
2024-05-27 02:14:55 -0300

    removendo inutilidades

--- frontend/src/views/CalculatorView.vue
+++ frontend/src/views/CalculatorView.vue
@@ -1,54 +1,16 @@
 <template>
 	<div class="about">
-		<h1>calculator screan</h1>
+		<img alt="Vue logo" src="../assets/logo.png">
+		<h1>Calculator Scream</h1>
 	</div>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
-import { useStore } from 'vuex';
 
-export default {
-	setup() {
-		const store = useStore();
-		const message = ref('');
-
-		onMounted(async () => {
-			try {
-				const response = await fetch('http://localhost:8000/api/user/', {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					credentials: 'include',
-				});
-				console.log(response)
-				if (!response.ok) {
-					message.value = 'You are not logged in!';
-				}else{
-					const data = await response.json();
-					
-					if (data.name) {
-						store.dispatch('login', data); // Despacha a ação de login
-						message.value = `Welcome ${data.nickName}!`;
-					}
-				}
-			} catch (error) {
-				throw new Error('There has been a problem with your fetch operation:', error);
-			}
-		});
-
-		return { message };
-	},
-};
 </script>
 
 <style scoped>
 .about {
 	color: white;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 100%;
 }
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,20 +1,19 @@
 <template>
 	<div class="home">
 		<img alt="Vue logo" src="../assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<h1>Home Scream</h1>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
 	name: 'HomeView',
-	components: {
-		HelloWorld
-	}
 }
 </script>
-
-<style scoped></style>
+<style scoped>
+.home {
+	color: white;
+}
+</style>
