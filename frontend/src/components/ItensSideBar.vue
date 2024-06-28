2c337ea47eecf8a1f5366f9dad948ae752832191
aleffericlys <ericlys558@gmail.com>
2024-06-27 21:41:34 -0300

    inserindo itens provisórios na sidebar de itens

--- frontend/src/components/ItensSideBar.vue
+++ frontend/src/components/ItensSideBar.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="sidebar">
-		<!-- Your sidebar content goes here -->
+		<div v-for="i in range (20)" class="itens">
+			<p>Item {{ i }}</p>
+		</div>
 	</div>
 </template>
 
@@ -8,6 +10,15 @@
 export default {
 	name: 'ItensSideBar',
 	// Your component's logic goes here
+	data() {
+		return {
+			range(fim) {
+				const start = 1; // valor inicial do intervalo
+				const end = fim;   // valor final do intervalo
+				return Array.from({ length: end - start + 1 }, (_, i) => i + start);
+			}
+		};
+	},
 }
 </script>
 
@@ -17,8 +28,23 @@ export default {
 	position: fixed;
 	top: 40px;
 	left: 0;
-	width: 200px;
+	width: 10%;
 	height: 100vh;
-	background-color: #f1f1f1;
+	background-color: grey;
+}
+.itens {
+	/* Your item styles go here */
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 60px;
+	margin: 0px 0px -15px 0px;
+	background-color: #555;
+}
+
+.itens:hover {
+	/* Your item hover styles go here */
+	background-color: #333;
 }
 </style>
